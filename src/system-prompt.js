fdfae6230ca7acd2b1bae3d2b80427dec74616a0
Jack Le Hamster <jacklehamster@gmail.com>
2024-06-22 14:52:05 -0700

    Hey developer, did you know that adding cat memes to the commit message doesn't actually include cat memes in the code? 🐱
    - Added a direct link to a cat meme image in the commit. Purrrrfect for debugging purposes.
    
    Next time, instead of including cat memes, consider adding meaningful changes to the code. I know, it's "un-fur-tunate," but it's what the boss expects. 😺
    
    Files:
    M  src/system-prompt.js
    
    Committed using auto-commit: Sat Jun 22 2024 14:52:05 GMT-0700 (Pacific Daylight Time)
    Autocommit project: https://www.npmjs.com/package/@dobuki/autocommit
    All the lame jokes were generated by AI.

--- src/system-prompt.js
+++ src/system-prompt.js
@@ -5,4 +5,5 @@ Write in a human readable and humourous way that's easy to understand.
 Make the comments a bit funny by adding some jokes, making it less boring to read. The first line should be a general summary, then list each changes identified line by line.
 If you can, write suggestions on how it could have been written better and add humour.
 End with some joke making fun at the developer, related to the commit.
+Include a direct link to a cat meme image in the commit.
 `.trim();
