66f6a246f879d3ea4b9b08f9cdfc20281c4bb77a
Jack Le Hamster <jacklehamster@gmail.com>
2024-10-22 23:28:28 -0700

    Commit

--- src/open-ai.js
+++ src/open-ai.js
@@ -1,38 +1,40 @@
 import OpenAI from "openai";
 import { systemPrompt } from "./system-prompt.js";
 
-const openai = process.env.OPENAI_API_KEY?.length ? new OpenAI({
-  apiKey: process.env.OPENAI_API_KEY,
-  organization: process.env.OPENAI_ORGANIZATION,
-  project: process.env.OPENAI_PROJECT_AI_COMMIT, //  ai-commit
-}) : undefined;
+const openai = process.env.OPENAI_COMMIT_KEY?.length
+  ? new OpenAI({
+      apiKey: process.env.OPENAI_COMMIT_KEY,
+      organization: process.env.OPENAI_ORGANIZATION,
+      project: process.env.OPENAI_COMMIT_PROJECT,
+    })
+  : undefined;
 
 /**
  * @param {string} diff
  */
-export async function generateComments(
-    diff,
-    model = "gpt-3.5-turbo-1106") {
+export async function generateComments(diff, model = "gpt-3.5-turbo-1106") {
   if (!openai) {
     return "";
   }
   const allMessages = [
     {
-      "role": "system",
-      "content": systemPrompt,
+      role: "system",
+      content: systemPrompt,
     },
     {
-      "role": "user",
-      "content": diff,
-    }
+      role: "user",
+      content: diff,
+    },
   ];
-  
-  const response = await openai.chat.completions.create({
-    model,
-    messages: allMessages,
-  }, {
-  }); 
+
+  const response = await openai.chat.completions.create(
+    {
+      model,
+      messages: allMessages,
+    },
+    {}
+  );
   const comment = response.choices[0].message.content;
 
-  return comment; 
+  return comment;
 }
